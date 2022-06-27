ad8e73c64fd835107174d98263fd13c9350fed3a
Jose Vargas <ruedagato@gmail.com>
2022-06-26 21:35:46 -0500

    fix styles

--- src/components/Marco.tsx
+++ src/components/Marco.tsx
@@ -15,9 +15,13 @@ interface MarcoProps {
 }
 
 const Marco: React.FC<MarcoProps> = (props) => {
+    React.useEffect(() => {
+        const vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+    }, [])
     return (
-        <Box className="flex flex-col Marco">
-            <AppBar position="static">
+        <Box className="Marco">
+            <AppBar position="fixed">
                 <Toolbar>
                     <IconButton size="large" edge="start" color="inherit" aria-label="menu">
                         <MenuIcon />
@@ -28,7 +32,7 @@ const Marco: React.FC<MarcoProps> = (props) => {
                     </Typography>
                 </Toolbar>
             </AppBar>
-            <Box className="flex-1 overflow-auto">{props.children}</Box>
+            <Box className="h-full">{props.children}</Box>
         </Box>
     );
 };
